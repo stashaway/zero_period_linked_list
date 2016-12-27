28fa53c20f956222033898d0689cc24ac3535c13
Daniel Paschal <daniel.paschal@learningfuze.com>
2016-12-27 08:37:45 -0800

    added solution 1

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function linked_list(){
     //create new object
     // set new_obj val to data_payload
     // set new object next to null
+    var new_obj = {
+      value: data_payload,
+      next: null
+    }
     //if current is not null
       // set current next to new object 
     //else 
@@ -16,33 +20,67 @@ function linked_list(){
     //set current to new object
     //increment count
     //returns count
+    if(this.current!=null){
+      this.current.next = new_obj;
+    }
+    else{
+      this.head = new_obj;
+    }
+    this.current = new_obj;
+    this.count++;
+    return this.count;
+
   }
   this.get_current_value = function(){
+    if(this.is_list_empty()){
+      return false;
+    }
+    return this.current.value;
   	//return the value of the current link list node
   }
   this.get_next_value = function(){
+    if(this.is_list_empty()){
+      return false;
+    }
+    else if(this.current.next === null){
+      console.log('at end of list');
+      return false;
+    }
+    this.current = this.current.next;
+    return this.get_current_value();
   	//walk to the next item in the list
   	//returns the value of the item walked to
   	//if there are no other items, it returns false
   }
+  this.is_list_empty = function(){
+    if(this.current===null){
+      console.log('list is empty');
+      return true;
+    }    
+  }
   this.rewind = function(){
+    if(this.is_list_empty()){
+      return false;
+    }
+    this.current = this.head;
+    return true;
   	//moves the list pointer back to the beginning of the list
   	//returns true if accomplished, or false if the list is empty
   }
 }
 
 var list = new linked_list();
-list.rewind(); //returns false
-list.add_list_item(1); //returns 1
-list.add_list_item(3); //returns 2
-list.add_list_item(8); //returns 3
+console.log(list.rewind()); //returns false
+console.log(list.add_list_item(1)); //returns 1
+console.log(list.add_list_item(3)); //returns 2
+console.log(list.add_list_item(8)); //returns 3
 console.log(list);
 console.log(list.get_current_value()); //returns 8
-list.rewind(); //returns true
-list.get_current_value(); //returns 1
-list.get_next_value(); //returns 3
-list.get_current_value(); //returns 3
-list.get_next_value(); //returns 8
-list.get_next_value(); //returns false
+console.log(list.rewind()); //returns true
+console.log(list.get_current_value()); //returns 1
+console.log(list.get_next_value()); //returns 3
+console.log(list.get_current_value()); //returns 3
+console.log(list.get_next_value()); //returns 8
+console.log(list.get_next_value()); //returns false
 
 
